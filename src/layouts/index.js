faa95ba9aa20bba0cda2fdc9fbdeb756432547dd
soniauppal <sonia9uppal@gmail.com>
2018-04-03 19:34:12 -0700

    glyphicons

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -4,7 +4,7 @@ import Helmet from 'react-helmet'
 
 import Header from '../components/Header'
 import './index.css'
-import '../../public/static/css/bootstrap.css'
+import '../../static/bootstrap.css'
 
 const TemplateWrapper = ({ children }) => (
   <div>
