870ae6f9763fc53a9a6b0679a9b7dcb8a6198618
soniauppal <sonia9uppal@gmail.com>
2018-04-10 17:00:37 -0700

    awards intro

--- src/pages/awards.js
+++ src/pages/awards.js
@@ -1,2 +1,35 @@
 import React from 'react';
 import Link from 'gatsby-link';
+import '../css/bootstrap.css';
+import '../css/awards.scss';
+
+
+//const AwardsPage = () => (
+class AwardsPage extends React.Component {
+  render () {
+    return (
+      <div>
+        <div class="panel">
+            <h1 class="panel__title">Awards & Honors</h1>
+            <p class="panel__subtitle">In recent years, the Daily Cal has earned dozens of awards on a regional and national level for its
+              comprehensive and high-quality coverage of UC Berkeley, the city of Berkeley and the entire UC system. In 2016,
+              the Society of Professional Journalists declared the Daily Californian to be the best daily student newspaper in
+              California, Nevada, Arizona and Hawaii.
+            </p>
+        </div>
+        <div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+        <div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default AwardsPage;
